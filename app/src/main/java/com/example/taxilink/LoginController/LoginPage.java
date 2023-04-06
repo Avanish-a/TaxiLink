c56973dc9c3196a5ebd91475cf36ebf1b3485b13
r-yeh <r.yeh2008@gmail.com>
2023-04-06 18:54:12 -0400

    small changes made to LoginPage

--- app/src/main/java/com/example/taxilink/LoginController/LoginPage.java
+++ app/src/main/java/com/example/taxilink/LoginController/LoginPage.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.taxilink.R;
 import com.example.taxilink.TaxiSessionController.HomePage;
