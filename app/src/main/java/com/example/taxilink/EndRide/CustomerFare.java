d8d0f7e6f24cfa2155f07eb65dfd781f78225434
Sheridan Fong <45665563+sheridan-fong@users.noreply.github.com>
2023-04-08 23:41:22 -0400

    Updated CustomerFare and RatingFormPage
    Integrated front end and backend together.

--- app/src/main/java/com/example/taxilink/EndRide/CustomerFare.java
+++ app/src/main/java/com/example/taxilink/EndRide/CustomerFare.java
@@ -1,16 +1,40 @@
 package com.example.taxilink.EndRide;
 
 import java.util.Map;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.taxilink.R;
 
-public class CustomerFare {
+public class CustomerFare extends AppCompatActivity{
 
-    Double fare;
+    private double fare;
+    private double fareRate;
+    private double distance;
     Map<String, String> rideInformation;
 
-    private Double calculateFare(Map<String, String> rideInformation){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_fare_page);
 
+        // get the distance traveled and fare rate from the previous activity or user input
+        distance = 10.5;
+        fareRate = 2.5;
+
+        // calculate the fare based on distance and fare rate
+        fare = calculateFare(distance, fareRate);
+
+        // display the total fare on the screen
+        TextView fareTextView = findViewById(R.id.fareValue);
+        fareTextView.setText(String.format("$%.2f", fare));
+    }
+
+    // make an Onclick function
+
+    private double calculateFare(double distance, double fareRate){
         // use the maps ride info function
-        fare = 100.00;
+        fare = distance * fareRate;
         return fare;
     }
 
--- app/src/main/java/com/example/taxilink/EndRide/CustomerRatingFormPage.java
+++ app/src/main/java/com/example/taxilink/EndRide/CustomerRatingFormPage.java
@@ -1,6 +1,8 @@
 package com.example.taxilink.EndRide;
 
+import java.util.HashMap;
 import java.util.Map;
+
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,9 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -19,49 +23,50 @@ import com.example.taxilink.databinding.FragmentFirstBinding;
 
 
 
-public class CustomerRatingFormPage extends Fragment {
+public class CustomerRatingFormPage extends AppCompatActivity {
 
-//    private CustomerRatingFormPageBinding binding;
+
     private RatingBar ratingBar;
     private Button submitButton;
-
-    public CustomerRatingFormPage(){
-
-    }
+    Map<String, String> customerRatingInformation = new HashMap<String, String>();
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.rating_page, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rating_page);
 
-        ratingBar = view.findViewById(R.id.rating_bar);
-        submitButton = view.findViewById(R.id.submit_button);
+        ratingBar = findViewById(R.id.rating_bar);
+        submitButton = findViewById(R.id.submit_button);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 float rating = ratingBar.getRating();
-                Toast.makeText(getActivity(), "Thanks for rating your person " + rating + " stars!", Toast.LENGTH_SHORT).show();
+                String message = "Rating: " + rating;
+                Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+                toast.show();
+                System.out.println("clicked: "+rating);
+
+                // you will need to input code for submitting the rating to the database
+                customerRatingInformation.put("Customer1",Float.toString(rating));
+                submitRating(customerRatingInformation);
             }
         });
-
-        return view;
     }
 
 
-    Map<String, String> customerRatingInformation;
 
-    private Map<String, String> addRating(String rating){
-        return customerRatingInformation;
-    }
-
-    private Map<String, String> saveCustomeResponse(){
-        return customerRatingInformation;
-    }
+//
+//    private Map<String, String> addRating(String rating){
+//        return customerRatingInformation;
+//    }
+//
+//    private Map<String, String> saveCustomerResponse(){
+//        return customerRatingInformation;
+//    }
 
     private void submitRating(Map<String, String> customerRatingInformation){
-        System.out.println("test");
+        System.out.println("Rating submitted to database");
     }
 
     private String handleTouch(){
