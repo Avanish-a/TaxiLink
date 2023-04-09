e544ecceab779f673e5d7cd22d84c01c4e7f6bef
t-wing11 <Traviswing20@gmail.com>
2023-04-09 11:57:50 -0400

    conditional page flow for create account and request result page

--- app/src/main/java/com/example/taxilink/TaxiSessionController/RequestLinkPage.java
+++ app/src/main/java/com/example/taxilink/TaxiSessionController/RequestLinkPage.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -45,9 +46,15 @@ public class RequestLinkPage extends Fragment {
             public void onClick(View v) {
                 // call TaxiSessionController with a method to encrypt and submit data to
                 // requestLinkController
-                NavHostFragment.findNavController(RequestLinkPage.this)
-                        .navigate(R.id.action_RequestLink_to_RequestResultPage);
-
+                String enteredDestination = destination.getText().toString();
+                String enteredPickup = start.getText().toString();
+                if (!enteredDestination.isEmpty() && !enteredPickup.isEmpty()) {
+                    NavHostFragment.findNavController(RequestLinkPage.this)
+                            .navigate(R.id.action_RequestLink_to_RequestResultPage);
+                } else {
+                    Toast.makeText(RequestLinkPage.this.getContext(), "Please fill in a Destination and pickup location" +
+                            " Please try again.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
