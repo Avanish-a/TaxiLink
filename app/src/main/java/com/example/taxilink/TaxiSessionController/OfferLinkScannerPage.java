83f01c42c84572cc056ea4530e344065a0c1eac2
r-yeh <r.yeh2008@gmail.com>
2023-04-10 15:05:25 -0400

    Added conditional submission and linking to OfferLinkScannerPage

--- app/src/main/java/com/example/taxilink/TaxiSessionController/OfferLinkScannerPage.java
+++ app/src/main/java/com/example/taxilink/TaxiSessionController/OfferLinkScannerPage.java
@@ -4,24 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.taxilink.BaseEntity.Carpool;
 import com.example.taxilink.BaseEntity.Profile;
-import com.example.taxilink.databinding.OfferLinkPageBinding;
+import com.example.taxilink.R;
 import com.example.taxilink.databinding.OfferLinkScannerPageBinding;
-import com.example.taxilink.databinding.RequestLinkPageBinding;
 import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class OfferLinkScannerPage extends Fragment {
 
@@ -45,18 +45,34 @@ public class OfferLinkScannerPage extends Fragment {
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String carpoolID = generateCarpoolID();
-                Profile owner = new Profile("123456789", "John", "Smith", "admin@test.com", "01/01/2001", "1234567890");
-                List<Profile> members = new ArrayList<Profile>();
-                String taxiID = "001";
-                Boolean active = true;
-                String enteredDestination = destination.getText().toString();
-                int capacity = 4;
+                if (!destination.getText().toString().isEmpty()) {
+                    String carpoolID = generateCarpoolID();
+                    Profile owner = new Profile("123456789", "John", "Smith", "admin@test.com", "01/01/2001", "1234567890");
+                    List<Profile> members = new ArrayList<Profile>();
+                    String taxiID = "001";
+                    Boolean active = true;
+                    String enteredDestination = destination.getText().toString();
+                    int capacity = 4;
 
-                Carpool newOffer = new Carpool(carpoolID, owner, taxiID, enteredDestination, capacity);
-                // send offer to database
-                Toast.makeText(OfferLinkScannerPage.this.getContext(),
-                        "Offer created successfully", Toast.LENGTH_SHORT).show();
+                    Carpool newOffer = new Carpool(carpoolID, owner, taxiID, enteredDestination, capacity);
+                    // send offer to database
+                    Toast.makeText(OfferLinkScannerPage.this.getContext(),
+                            "Offer created successfully", Toast.LENGTH_SHORT).show();
+
+                    // for hardcoded response
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    NavHostFragment.findNavController(OfferLinkScannerPage.this)
+                            .navigate(R.id.action_ScannedOfferLink_to_OfferAcceptDeny);
+                }
+                else {
+                    Toast.makeText(OfferLinkScannerPage.this.getContext(),
+                            "Destination cannot be empty.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
