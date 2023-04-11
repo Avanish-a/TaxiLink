5fb208a7fef21fccbcf69ab21e6ae0277a9bc317
Sheridan Fong <45665563+sheridan-fong@users.noreply.github.com>
2023-04-11 01:20:50 -0400

    Updated Page
    API Key isn't working rn

--- app/src/main/java/com/example/taxilink/RideInformation/DirectionPage.java
+++ app/src/main/java/com/example/taxilink/RideInformation/DirectionPage.java
@@ -12,9 +12,24 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.taxilink.R;
 import com.example.taxilink.databinding.MapsDirectionPageBinding;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
 
-public class DirectionPage extends Fragment {
+public class DirectionPage extends Fragment implements OnMapReadyCallback{
     private MapsDirectionPageBinding binding;
+    private GoogleMap mMap;
+    private MapView mapView;
+    private GeoApiContext geoApiContext;
+
+
 
 
     @Override
@@ -23,8 +38,19 @@ public class DirectionPage extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         binding = MapsDirectionPageBinding.inflate(inflater, container, false);
+        mapView = binding.mapView;
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
         return binding.getRoot();
 
+    }
+
+    // Add the required onMapReady() method
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        mMap = googleMap;
+        getDirections();
     }
 
     @Override
@@ -38,6 +64,37 @@ public class DirectionPage extends Fragment {
                         .navigate(R.id.action_mapsDirectionPage_to_HomePage);
             }
         });
+
+        geoApiContext = new GeoApiContext.Builder()
+                .apiKey("AIzaSyC42oZdvHGae7r2JWS9jZ3mKA1FV37zSEU")
+                .build();
+
+    }
+
+    private void getDirections(){
+        String endLocation = "1280 Main Street West, Hamilton, ON";
+        String startLocation = "University Plaza, Osler Drive, Dundas, ON";
+
+        DirectionsApi.newRequest(geoApiContext)
+                .mode(TravelMode.DRIVING)
+                .origin(startLocation)
+                .destination(endLocation)
+                .setCallback(new PendingResult.Callback<DirectionsResult>() {
+                    @Override
+                    public void onResult(DirectionsResult result) {
+                        if(result.routes.length > 0){
+                            mMap.clear();
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(result.routes[0].legs[0].startLocation.lat, result.routes[0].legs[0].startLocation.lng)).title(result.routes[0].legs[0].startAddress));
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(result.routes[0].legs[0].endLocation.lat, result.routes[0].legs[0].endLocation.lng)).title(result.routes[0].legs[0].endAddress));
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Throwable e) {
+                        System.out.println(" a failure has occurred");
+                    }
+                });
     }
 
 }
